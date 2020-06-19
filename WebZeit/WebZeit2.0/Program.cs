7a6cad83d589ff1ed37d6c0390fa7337da3d7508
Tobias Reipert <tobiasbmx@freenet.de>
2020-06-19 13:25:46 +0200

    commit

--- WebZeit/WebZeit2.0/Program.cs
+++ WebZeit/WebZeit2.0/Program.cs
@@ -1,20 +1,25 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.Data;
+using WebZeit2.Models;
+using WebZeit2.Pages;
 
-namespace WebApplication3
+namespace WebZeit2
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            MainController.Abteilungen = MainController.GetAbteilungen();
             CreateHostBuilder(args).Build().Run();
         }
+
+        
+
+        
+
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
@@ -22,5 +27,7 @@ namespace WebApplication3
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+       
     }
 }
--- WebZeit/WebZeit2.0/Startup.cs
+++ WebZeit/WebZeit2.0/Startup.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace WebApplication3
+namespace WebZeit2
 {
     public class Startup
     {
@@ -24,11 +26,21 @@ namespace WebApplication3
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddMemoryCache();
+            services.Configure<IISOptions>(options =>
+            {
+                options.ForwardClientCertificate = false;
+            });
+            services.AddSession(options => {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSession();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -37,12 +49,13 @@ namespace WebApplication3
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                
             }
+
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            
             app.UseRouting();
 
             app.UseAuthorization();
